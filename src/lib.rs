ef3b0da25e50fdad688b65ece155e3ce8acb9f91
Riccardo Fano <rcr.fano@gmail.com>
2024-11-10 14:24:45 +0100

    Context works now

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 use thiserror::Error;
 use winnow::{
     ascii::digit0,
-    combinator::{alt, empty, preceded, separated_pair},
+    combinator::{alt, empty, separated_pair},
+    error::{
+        StrContext::{Expected, Label},
+        StrContextValue::{CharLiteral, Description, StringLiteral},
+    },
     token::take_while,
     PResult, Parser,
 };
@@ -16,9 +20,10 @@ pub enum DiceParseError {
 pub enum DieKind {
     /// Standard dice with number of sides
     Standard(i32),
-    /// Fate or Fudge die with number of - sides
-    /// F.2 -> Fate.Sides
-    Fate(i32),
+    /// Fudge/Fate die with 4 blanks, 1 plus, 1 minus
+    Fudge1,
+    /// Fudge/Fate die with 2 blanks, 2 plus, 2 minus
+    Fudge2,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -35,20 +40,40 @@ impl Dice {
 
 fn dice(input: &mut &str) -> PResult<Dice> {
     separated_pair(
-        alt((non_zero_start_number, empty.map(|_| 1))),
-        'd',
+        alt((non_zero_start_number, empty.map(|_| 1)))
+            .context(Label("dice quantity"))
+            .context(Expected(Description(
+                "quantity must either be a number without leading 0s or empty (to indicate 1 die)",
+            ))),
+        'd'.context(Label("d")).context(Expected(CharLiteral('d'))),
         die_kind,
     )
     .map(|(quantity, kind)| Dice { quantity, kind })
+    .context(Label("Dice"))
     .parse_next(input)
 }
 
 fn die_kind(input: &mut &str) -> PResult<DieKind> {
     alt((
-        '%'.map(|_| DieKind::Standard(100)),
-        alt((preceded("F.", non_zero_start_number), 'F'.map(|_| 2))).map(DieKind::Fate),
-        non_zero_start_number.map(DieKind::Standard),
+        '%'.map(|_| DieKind::Standard(100))
+            .context(Label("Percentile die"))
+            .context(Expected(CharLiteral('%'))),
+        "F.1"
+            .map(|_| DieKind::Fudge1)
+            .context(Label("Fudge die variant"))
+            .context(Expected(StringLiteral("F.1"))),
+        "F.2"
+            .map(|_| DieKind::Fudge2)
+            .context(Label("Standard Fudge die"))
+            .context(Expected(StringLiteral("F.2"))),
+        'F'.map(|_| DieKind::Fudge2)
+            .context(Label("Standard Fudge die"))
+            .context(Expected(CharLiteral('F'))),
+        non_zero_start_number
+            .map(DieKind::Standard)
+            .context(Label("Standard")),
     ))
+    .context(Label("Die kind"))
     .parse_next(input)
 }
 
@@ -84,9 +109,23 @@ mod tests {
     }
 
     #[test]
-    fn test_one_default_fate_dice() {
+    fn test_one_standard_fudge_die() {
         let dice = Dice::parse("1dF").unwrap();
         assert_eq!(dice.quantity, 1);
-        assert_eq!(dice.kind, DieKind::Fate(2))
+        assert_eq!(dice.kind, DieKind::Fudge2)
+    }
+
+    #[test]
+    fn test_one_standard_fudge_die_dot_notation() {
+        let dice = Dice::parse("1dF.2").unwrap();
+        assert_eq!(dice.quantity, 1);
+        assert_eq!(dice.kind, DieKind::Fudge2)
+    }
+
+    #[test]
+    fn test_one_variant_fudge_die() {
+        let dice = Dice::parse("1dF.1").unwrap();
+        assert_eq!(dice.quantity, 1);
+        assert_eq!(dice.kind, DieKind::Fudge1)
     }
 }
