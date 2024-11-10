e84ec3992792fa7847210089be674abaa2cbed5d
Riccardo Fano <rcr.fano@gmail.com>
2024-11-10 16:09:28 +0100

    Add ability to roll the dice that was parsed first, fails if there are more

---
+++ src/roll.rs
@@ -0,0 +1,38 @@
+use rand::random;
+
+use crate::parse::{Dice, DieKind};
+
+pub fn roll(input: &str) -> String {
+    // TODO: support multiple dice
+    let dice = match Dice::parse(input) {
+        Ok(d) => d,
+        Err(e) => return e,
+    };
+
+    let mut rolled = Vec::with_capacity(dice.quantity as usize);
+    for _ in 0..dice.quantity {
+        rolled.push(roll_dice(random(), dice.kind));
+    }
+
+    format!("{:?}", rolled)
+}
+
+pub fn roll_dice(random_value: f32, die_kind: DieKind) -> i32 {
+    match die_kind {
+        DieKind::Standard(sides) => (random_value * sides as f32).ceil() as i32,
+        DieKind::Fudge1 => [-1, 0, 0, 0, 0, 1][(random_value * 6_f32).floor() as usize],
+        DieKind::Fudge2 => (random_value * 3_f32).floor() as i32 - 1,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rolling() {
+        dbg!(roll("100dF.1"));
+
+        todo!()
+    }
+}
